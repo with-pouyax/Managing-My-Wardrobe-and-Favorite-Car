adbf91d44b9b7e49dcb30ef2c5cb48b0cdd4fb46
pouyax-experience <75066687+with-pouyax@users.noreply.github.com>
2023-11-15 17:08:49 +0100

    Create wardrobeAndCar.js

---
+++ wardrobeAndCar.js
@@ -0,0 +1,13 @@
+var clothes = [];
+clothes.push("t-shirt","Dress","Sneakers","Sweater","Jeans");
+clothes.pop();
+clothes.push("skirt");
+
+
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.color = "green";
+favCar.covertible = true;
+
+console.log(favCar);
